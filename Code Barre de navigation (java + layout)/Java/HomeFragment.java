ad98c3651584808a001239aeaea69550c5e75d65
Adrien <adrien.laigle35@gmail.com>
2019-12-22 14:21:14 +0100

    Code

---
+++ Code Barre de navigation (java + layout)/Java/HomeFragment.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HomeFragment extends Fragment {
+
+    Button button;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        Button button = (Button) view.findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(), Main3Activity.class);
+                in.putExtra("some", "some data");
+                startActivity(in);
+            }
+        });
+
+        return view;
+        }
+}
---
+++ Code Barre de navigation (java + layout)/Java/Main2Activity.java
@@ -0,0 +1,61 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        new AlertDialog.Builder(Main2Activity.this).setTitle("Bonjour")
+                .setMessage("Bienvenue dans Delivery Manager")
+                .show();
+
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemReselectedListener(navListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemReselectedListener navListener =  //Barre de navigation
+            new BottomNavigationView.OnNavigationItemReselectedListener() {
+                @Override
+                public void onNavigationItemReselected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.nav_home:    //Différents onglets (Accueil, Map, Statistique et Contact)
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_map:
+                            selectedFragment = new MapFragment();
+                            break;
+                        case R.id.nav_stat:
+                            selectedFragment = new StatsFragment();
+                            break;
+                        case R.id.nav_contact:
+                            selectedFragment = new ContactFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return;
+                }
+            };
+
+}
